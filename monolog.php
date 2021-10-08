71efcd7a6b480e092deb1cc59a4f41e9491aff94
Denis789-4 <b0tt0miezz@gmail.com>
2021-10-09 00:09:30 +0300

    fix logger

--- monolog.php
+++ monolog.php
@@ -7,8 +7,11 @@ use Monolog\Handler\StreamHandler;
 
 // create a log channel
 $log = new Logger('DEBUG LOGGER');
-$log->pushHandler(new StreamHandler(__DIR__.'/logs/log.txt', Logger::WARNING));
+$log->pushHandler(new StreamHandler(__DIR__.'/log.txt', Logger::WARNING));
 
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+$log->error('q');
+$log->error('w');
+$log->error('e');
